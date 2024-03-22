1f07825221f5cc114cab89a62d03d41b2126cabd
Aswanth Vc <aswanth.abam@gmail.com>
2024-03-22 16:03:55 +0530

    feat (server): minor changes and docstring

--- server/utils.py
+++ server/utils.py
@@ -6,30 +6,54 @@ YOLO_CFG = "./yolo/yolov3.cfg"
 COCO_NAMES = "./yolo/coco.names"
 
 def load_yolo():
+	"""
+	load the yolo model and classes
+	:return: yolo model, classes, output layers
+	"""
 	net = cv2.dnn.readNet(YOLO_WEIGHTS, YOLO_CFG)
 	classes = []
 	with open(COCO_NAMES, "r") as f:
 		classes = [line.strip() for line in f.readlines()] 
 	
 	output_layers = [layer_name for layer_name in net.getUnconnectedOutLayersNames()]
-	colors = np.random.uniform(0, 255, size=(len(classes), 3))
-	return net, classes, colors, output_layers
+	return net, classes, output_layers
 
 
-def load_image(img_path):   
+def load_image(img_path): 
+	"""
+	Load the image and resize it to 0.4 of its original size
+	:param img_path: path to the image
+	:return: image, height, width, channels, original image
+	"""  
 	img = cv2.imread(img_path)
 	org_img = img.copy()
 	img = cv2.resize(img, None, fx=0.4, fy=0.4)
 	height, width, channels = img.shape
 	return img, height, width, channels, org_img
 
-def detect_objects(img, net, outputLayers):			
+def detect_objects(img, net, outputLayers):
+	"""
+	Detect objects in the image
+	:param img: image
+	:param net: yolo model
+	:param outputLayers: output layers
+	:return: blob and outputs
+	"""
+
 	blob = cv2.dnn.blobFromImage(img, scalefactor=0.00392, size=(320, 320), mean=(0, 0, 0), swapRB=True, crop=False)
 	net.setInput(blob)
 	outputs = net.forward(outputLayers)
 	return blob, outputs
 
 def get_box_dimensions(outputs, height, width):
+	"""
+	Get the dimensions of the bounding box
+	:param outputs: yolo model outputs
+	:param height: height of the image
+	:param width: width of the image
+	:return: bounding box dimensions
+	"""
+
 	boxes = []
 	confs = []
 	class_ids = []
@@ -51,7 +75,16 @@ def get_box_dimensions(outputs, height, width):
 	return boxes, confs, class_ids
 
 
-def draw_labels(boxes, confs, colors:list, class_ids, classes, img,  width_ration=1, height_ratio=1): 
+def draw_labels(boxes, confs, class_ids, classes, img): 
+	"""
+	Draw the labels on the image
+	:param boxes: bounding boxes
+	:param confs: confidence
+	:param class_ids: class ids
+	:param classes: classes
+	:param img: image
+	:return: image with labels
+	"""
 	indexes = cv2.dnn.NMSBoxes(boxes, confs, 0.5, 0.4)
 	font = cv2.FONT_HERSHEY_PLAIN
 	label_counter = {
@@ -74,7 +107,6 @@ def draw_labels(boxes, confs, colors:list, class_ids, classes, img,  width_ratio
 	for i in range(len(boxes)):
 		if i in indexes:
 			x, y, w, h = boxes[i]
-			x,y,w,h = x * width_ration, y * height_ratio, w * width_ration, h * height_ratio
 			label = str(classes[class_ids[i]])
 			if label in label_counter.keys():
 				label_counter[label] += 1
@@ -88,15 +120,28 @@ def draw_labels(boxes, confs, colors:list, class_ids, classes, img,  width_ratio
 	return img, label_counter
 
 def image_detect(img_path): 
-	model, classes, colors, output_layers = load_yolo()
+	"""
+	Detect objects in the image
+	:param img_path: path to the image
+	:return: image with labels
+	"""
+
+	model, classes, output_layers = load_yolo()
 	image, height, width, channels, org_img = load_image(img_path)
-	blob, outputs = detect_objects(image, model, output_layers)
+	_, outputs = detect_objects(image, model, output_layers)
 	boxes, confs, class_ids = get_box_dimensions(outputs, height, width)
 	b = [convert_rectangle((height, width), (org_img.shape[0],org_img.shape[1]), box) for box in boxes]
-	img, label_counter = draw_labels(b, confs, colors, class_ids, classes, org_img)
+	img, label_counter = draw_labels(b, confs, class_ids, classes, org_img)
 	return img, label_counter
 
 def convert_rectangle(resized_img_shape, original_img_shape, rectangle):
+	"""
+	Convert rectangle from resized image to original image
+	:param resized_img_shape: shape of resized image
+	:param original_img_shape: shape of original image
+	:param rectangle: rectangle
+	:return: rectangle in original image
+	"""
 	print(resized_img_shape, original_img_shape, rectangle)
 	scale_x = original_img_shape[1] / resized_img_shape[1]
 	scale_y = original_img_shape[0] / resized_img_shape[0]
@@ -113,5 +158,12 @@ def convert_rectangle(resized_img_shape, original_img_shape, rectangle):
 	return (x_original, y_original, width_original, height_original)
 
 def save_image(img, path):
+	"""
+	Save the image
+	:param img: image
+	:param path: path to save the image
+	:return: path
+	"""
+	
 	cv2.imwrite(path, img)
 	return path
