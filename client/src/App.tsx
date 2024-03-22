e8c6650570e93d08ed6fe3e51549421a229a18dd
Aswanth Vc <aswanth.abam@gmail.com>
2024-03-22 17:29:31 +0530

    feat(client) : Minor changes and optimizations

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,37 @@
-import "./App.css"
-import { Home } from './pages/Home/Home'
+import { Route, Router, Routes } from "react-router";
+import "./App.css";
+import { TopBar } from "./components/TopBar/TopBar";
+import { Home } from "./pages/Home/Home";
 
 function App() {
   return (
     <div className="app">
-      <Home />
+      <TopBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/about" element={<div>About</div>} />
+        <Route path="/contact" element={<div>Contact</div>} /> */}
+        <Route
+          path="*"
+          element={
+            <div
+              style={{
+                width: "99vw",
+                height: "100vh",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <h1 style={{
+                fontWeight: "bold",
+              }}>Not Found 404</h1>
+            </div>
+          }
+        />
+      </Routes>
     </div>
-  )
-} 
+  );
+}
 
-export default App
+export default App;
