f8a51dcfc9c4fea3ed48480000704b6394ec768a
Aswanth Vc <aswanth.abam@gmail.com>
2024-03-22 13:56:39 +0530

    fix : cors headers

--- server/app.py
+++ server/app.py
@@ -3,12 +3,14 @@ from response import Response
 from uuid import uuid4
 import os
 import utils
+from flask_cors import CORS
 
 app = Flask(__name__, static_url_path='/media/', static_folder='media/') 
 UPLOAD_FOLDER = 'media/images/'
 RESULT_FOLDER = 'media/results/'
 CARS_CASCADE = 'cascades/cars.xml'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+CORS(app)
 
 @app.route('/process', methods = ['GET', 'POST']) 
 def home(): 
@@ -29,7 +31,8 @@ def home():
             utils.save_image(img, os.path.join(RESULT_FOLDER, filename))
             return Response("File uploaded successfully", {
                 "result": result,
-                "url": f"{request.url_root}media/results/{filename}"
+                "result_url": f"{request.url_root}media/results/{filename}",
+                "upload_url": f"{request.url_root}media/images/{filename}"
             }).send_success_response(200)
     return Response("Method not allowed", {}).send_failiure_response(405)
   
